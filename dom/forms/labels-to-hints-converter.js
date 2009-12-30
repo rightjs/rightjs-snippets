618d80e27bc9868ea835ce96eafd973cac3ae9ab
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2009-12-30 15:46:11 +0300

    Steven's labels-to-hints converter

---
+++ dom/forms/labels-to-hints-converter.js
@@ -0,0 +1,29 @@
+/**
+ * This little script grabs labels and converts them into hint texts
+ * on the input elements that assigned to the labels
+ *
+ * Copyright (C) 2009 Steven Garcia
+ */
+var formHelper = new Class({ 
+  addHints:  function() {
+    $$('.add_hints label').each( function(label) {
+      var input = $(label.get('for'));
+      var text = label.innerHTML;
+      label.hide();
+
+      $ext(input, {
+        showHint: function() {
+          if (this.value.blank())
+            this.addClass('hint').setValue(text);
+        },
+        hideHint: function() {
+          if (this.value == text)
+            this.removeClass('hint').setValue('');
+        }
+      }).on({
+        focus: 'hideHint',
+        blur:  'showHint'
+      }).showHint();
+    });
+  }
+});
